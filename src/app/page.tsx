2cb724fe9be6b4a144965b124e0a584e51aeb891
Hideaki Takahashi <t.takashi0602@gmail.com>
2023-11-04 01:58:44 +0900

    feat: add career section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -27,7 +27,57 @@ export default function Home() {
         </ul>
       </section>
       <section className={styles.section}>
-        <h2>職歴</h2>
+        <h2>学歴・職歴</h2>
+        <ul>
+          <ListItem
+            text="株式会社ゆめみ"
+            isLink
+            href="https://www.yumemi.co.jp/"
+            period="（2021/04 ~ 現在）"
+            employmentStatus="フルタイム"
+          >
+            <ul>
+              <ListItem text="Nuxt / Next.jsを使用したWebアプリの開発" />
+              <ListItem text="Flutterを使用したネイティブアプリの開発" />
+            </ul>
+          </ListItem>
+          <ListItem
+            text="株式会社インパス"
+            href="https://www.impath.co.jp/"
+            isLink
+            period="（2019/12 ~ 2021/03）"
+            employmentStatus="パートタイム"
+          >
+            <ul>
+              <ListItem text="Reactを使用したWeb Viewでのネイティブアプリの開発" />
+              <ListItem text="Firebaseを使用したWebアプリの開発" />
+            </ul>
+          </ListItem>
+          <ListItem
+            text="株式会社chatbox"
+            href="https://chatbox-inc.com/"
+            isLink
+            period="（2017/07 ~ 2019/12）"
+            employmentStatus="パートタイム"
+          >
+            <ul>
+              <ListItem text="Laravelを使用したREST APIの開発" />
+              <ListItem text="Nuxtを使用したWebサイトやWebアプリの開発" />
+            </ul>
+          </ListItem>
+          <ListItem
+            text="大阪情報コンピュータ専門学校"
+            href="https://www.oic.ac.jp/"
+            isLink
+            period="（2017/04 ~ 2021/03）"
+          >
+            <ul>
+              <ListItem text="ITの基礎を学び、開発や資格の取得に励む" />
+              <ListItem text="ITCreate部に所属し、LT会や勉強会、カンファレンスのスタッフを行う" />
+              <ListItem text="部員同士でチームを組んでハッカソンやコンテストに出場する" />
+            </ul>
+          </ListItem>
+        </ul>
       </section>
       <section className={styles.section}>
         <h2>リンク</h2>
